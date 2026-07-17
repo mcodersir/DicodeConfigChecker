931068283f5d8b34b99d5e3cebc4b91be23e3051
M_CODER <84375305+mcodersir@users.noreply.github.com>
2026-07-17 23:02:47 +0330

    Release v1.4.9 personal subscriptions

--- desktop.py
+++ desktop.py
@@ -8,7 +8,7 @@ import platform
 import time
 from typing import Callable
 
-VERSION = "1.4.1"
+VERSION = "1.4.9"
 os.environ.setdefault("QT_ENABLE_HIGHDPI_SCALING", "1")
 os.environ.setdefault("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 os.environ.setdefault("QT_SCALE_FACTOR_ROUNDING_POLICY", "PassThrough")
